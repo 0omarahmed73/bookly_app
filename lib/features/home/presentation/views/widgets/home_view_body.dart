f739054d44ffcf2cfcafe1063b33358ceb74873f
Omar Ahmed <0omarahmed73@gmail.com>
2024-06-23 00:13:04 +0300

    add font family to UI

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -3,6 +3,7 @@ import 'package:bookly/features/home/presentation/views/widgets/custom_app_bar.d
 import 'package:bookly/features/home/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:bookly/features/home/presentation/views/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
