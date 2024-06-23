7d1f9d310d34fcaf42126faa63a43d34d645b537
Omar Ahmed <0omarahmed73@gmail.com>
2024-06-23 18:48:45 +0300

    create body details view

---
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BookDetailsViewBody extends StatelessWidget {
+  const BookDetailsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'BookDetailsViewBody is working',
+        style: TextStyle(fontSize: 20),
+      ),
+    );
+  }
+}
